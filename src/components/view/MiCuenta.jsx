d199f25387fa45a9daf3f212cb43c0e0fbbdd38a
Mauro Villagra <maurovillagra257@gmail.com>
2023-08-28 13:32:35 -0300

    second commit

--- src/components/view/MiCuenta.jsx
+++ src/components/view/MiCuenta.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./MiCuenta.css";
+import "./Micuenta.css";
 function MiCuenta({ usuarioActivo }) {
   const [bandera, setBandera] = useState("");
   useEffect(() => {
